2c14beae5d601aad1d6f3767723be2042c05b6ae
phuc <phuct6776@gmail.com>
2024-11-03 16:56:06 +0700

    update Itemcard

--- src/pages/Cart/ItemCard.js
+++ src/pages/Cart/ItemCard.js
@@ -16,7 +16,7 @@ const ItemCard = ({ item }) => {
           onClick={() => dispatch(deleteItem(item._id))}
           className="text-primeColor hover:text-red-500 duration-300 cursor-pointer"
         />
-        <img className="w-32 h-32" src={item.image} alt="productImage" />
+        <img className="w-32 h-32" src={item.image} alt="Hình ảnh sản phẩm" />
         <h1 className="font-titleFont font-semibold">{item.name}</h1>
       </div>
       <div className="col-span-5 mdl:col-span-3 flex items-center justify-between py-4 mdl:py-0 px-4 mdl:px-0 gap-6 mdl:gap-0">
