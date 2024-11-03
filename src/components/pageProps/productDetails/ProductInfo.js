15c8b9b5239c267c6410a055569a04559ded4733
chinhhuynh192 <chinhhdde160540@fpt.edu.vn>
2024-11-03 17:03:06 +0700

    update

--- src/components/pageProps/productDetails/ProductInfo.js
+++ src/components/pageProps/productDetails/ProductInfo.js
@@ -9,9 +9,9 @@ const ProductInfo = ({ productInfo }) => {
       <h2 className="text-4xl font-semibold">{productInfo.productName}</h2>
       <p className="text-xl font-semibold">${productInfo.price}</p>
       <p className="text-base text-gray-600">{productInfo.des}</p>
-      <p className="text-sm">Be the first to leave a review.</p>
+      <p className="text-sm">Hãy là người đầu tiên để lại đánh giá.</p>
       <p className="font-medium text-lg">
-        <span className="font-normal">Colors:</span> {productInfo.color}
+        <span className="font-normal">Màu sắc:</span> {productInfo.color}
       </p>
       <button
         onClick={() =>
@@ -29,11 +29,10 @@ const ProductInfo = ({ productInfo }) => {
         }
         className="w-full py-4 bg-primeColor hover:bg-black duration-300 text-white text-lg font-titleFont"
       >
-        Add to Cart
+        Thêm vào giỏ hàng
       </button>
       <p className="font-normal text-sm">
-        <span className="text-base font-medium"> Categories:</span> Spring
-        collection, Streetwear, Women Tags: featured SKU: N/A
+        <span className="text-base font-medium">Danh mục:</span> Bộ sưu tập mùa xuân, Thời trang đường phố, Phụ nữ Tags: nổi bật SKU: N/A
       </p>
     </div>
   );
