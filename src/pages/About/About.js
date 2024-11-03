3c08c6b45ca6ffe2dc77702038942af5f458fc35
phuc <phuct6776@gmail.com>
2024-11-03 16:32:42 +0700

    update pages với about 18/10

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -10,17 +10,15 @@ const About = () => {
   }, [location]);
   return (
     <div className="max-w-container mx-auto px-4">
-      <Breadcrumbs title="About" prevLocation={prevLocation} />
+      <Breadcrumbs title="Giới thiệu" prevLocation={prevLocation} />
       <div className="pb-10">
         <h1 className="max-w-[600px] text-base text-lightText mb-2">
           <span className="text-primeColor font-semibold text-lg">Orebi</span>{" "}
-          is one of the world's leading ecommerce brands and is internationally
-          recognized for celebrating the essence of classic Worldwide cool
-          looking style.
+          là một trong những thương hiệu thương mại điện tử hàng đầu thế giới và được công nhận trên toàn cầu vì việc tôn vinh bản chất của phong cách cổ điển, thời trang và đầy phong cách.
         </h1>
         <Link to="/shop">
           <button className="w-52 h-10 bg-primeColor text-white hover:bg-black duration-300">
-            Continue Shopping
+            Tiếp tục mua sắm
           </button>
         </Link>
       </div>
